7073f3c4d50eb179b209ac34179fc35ad722e977
Tinayo001 <keiyatinayo@gmail.com>
2024-11-21 11:20:29 +0300

    create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,33 +1,34 @@
 #!/usr/bin/env python3
-"""DB module
-"""
+# -*- coding: utf-8 -*-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-from user import Base, User
-from typing import TypeVar
+from sqlalchemy.exc import IntegrityError, InvalidRequestError
 
-VALID_FIELDS = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
+from user import Base
+from user import User
 
 
 class DB:
-    """DB class
+    """
+    DB class
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
+        """
+        Memoized session object
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -36,36 +37,44 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """
-        Adds a new user to the Database.
+        Create and add a new user to the User table
         """
-        if not email or not hashed_password:
-            return
-        user = User(email=email, hashed_password=hashed_password)
-        session = self._session
-        session.add(user)
-        session.commit()
-        return user
+        if email is None or hashed_password is None:
+            return None
+
+        try:
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
+            self._session.commit()
+            return user
+        except IntegrityError:
+            self._session.rollback()
+            return ValueError("User already exists")
 
     def find_user_by(self, **kwargs) -> User:
         """
-        Finds a User in the Database.
+        Find a user in the User table in the database
         """
-        if not kwargs or any(x not in VALID_FIELDS for x in kwargs):
-            raise InvalidRequestError
-        session = self._session
         try:
-            return session.query(User).filter_by(**kwargs).one()
-        except Exception:
-            raise NoResultFound
+            return self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound as e:
+            raise e
+        except InvalidRequestError as error:
+            raise error
 
     def update_user(self, user_id: int, **kwargs) -> None:
         """
-        updating a user in the database
+        Update a user in the User table in the database
         """
-        session = self._session
+        if user_id is None or kwargs is None:
+            return None
+
         user = self.find_user_by(id=user_id)
-        for k, v in kwargs.items():
-            if k not in VALID_FIELDS:
-                raise ValueError
-            setattr(user, k, v)
-        session.commit()
+
+        for attr, value in kwargs.items():
+            if not hasattr(user, attr):
+                raise ValueError("Invalid attribute")
+            setattr(user, attr, value)
+
+        self._session.commit()
+        return None
